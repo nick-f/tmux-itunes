5df43f50c235a3e587713febc6b07fb1a580a76d
Nick Freeland <nick@approachvector.net>
2019-10-12 12:30:43 +1100

    Add support for macOS Catalina
    The latest release of macOS, 10.15 Catalina, replaces the iTunes app
    with a new Music app. The script needs to be updated in order to
    continue working.

--- scripts/current_track.sh
+++ scripts/current_track.sh
@@ -1,19 +1,38 @@
 #!/usr/bin/env bash
 
 CURRENT_TRACK_STR=$(osascript <<EOF
-  tell application "iTunes"
-    if player state is stopped
-      return ""
-    else
-      if player state is paused then
-        set playingState to "❚❚"
-      else
-        set playingState to "▶"
-      end if
+  set _versionString to system version of (system info)
+  considering numeric strings
+    if _versionString ≥ "10.15" then
+      tell application "Music"
+        if player state is stopped then
+          return ""
+        else
+          if player state is paused then
+            set playingState to "❚❚"
+          else
+            set playingState to "▶"
+          end if
 
-      return playingState & " " & name of current track & " - " & artist of current track
+          return playingState & " " & name of current track & " - " & artist of current track
+        end if
+      end tell
+    else
+      tell application "iTunes"
+        if player state is stopped then
+          return ""
+        else
+          if player state is paused then
+            set playingState to "❚❚"
+          else
+            set playingState to "▶"
+          end if
+
+          return playingState & " " & name of current track & " - " & artist of current track
+        end if
+      end tell
     end if
-  end tell
+  end considering
 EOF)
 
 echo $CURRENT_TRACK_STR
